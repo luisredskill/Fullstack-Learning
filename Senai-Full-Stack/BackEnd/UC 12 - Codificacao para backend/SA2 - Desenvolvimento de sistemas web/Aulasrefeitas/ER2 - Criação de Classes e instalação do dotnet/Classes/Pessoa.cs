f5a50a7d407083dcf35bb39c1126f626d803ded8
Luis Carlos Barberino Ribeiro <jklcw71@gmail.com>
2022-03-29 09:25:21 -0300

    heranca

---
+++ Senai-Full-Stack/BackEnd/UC 12 - Codificacao para backend/SA2 - Desenvolvimento de sistemas web/Aulasrefeitas/ER2 - Criação de Classes e instalação do dotnet/Classes/Pessoa.cs
@@ -0,0 +1,9 @@
+using ER2___Criação_de_Classes_e_instalação_do_dotnet.Interfaces;
+
+namespace ER2___Criação_de_Classes_e_instalação_do_dotnet.Classes
+{
+    public class Pessoa : IPessoa 
+    {
+        
+    }
+}
---
+++ Senai-Full-Stack/BackEnd/UC 12 - Codificacao para backend/SA2 - Desenvolvimento de sistemas web/Aulasrefeitas/ER2 - Criação de Classes e instalação do dotnet/Interfaces/IPessoa.cs
@@ -0,0 +1,9 @@
+namespace ER2___Criação_de_Classes_e_instalação_do_dotnet.Interfaces
+{
+    public interface IPessoa
+    {
+         float PagarImposto(float rendimento){
+             
+         }
+    }
+}
